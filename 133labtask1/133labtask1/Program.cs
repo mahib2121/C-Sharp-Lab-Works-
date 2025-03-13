b88099f57dbafc638bb609e05956bc5caefd841f
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2025-03-13 11:16:26 +0600

    Add files via upload

---
+++ 133labtask1/133labtask1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _133labtask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter firstName ");
+            string firstName = Console.ReadLine();
+            
+            Console.WriteLine("Enter LastName ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Birthday");
+            string birthdate = Console.ReadLine();
+
+            Console.WriteLine("Enter addressLine1 ");
+            string addressLine1 = Console.ReadLine();
+
+            Console.WriteLine("Enter addressLine2 ");
+            string addressLine2 = Console.ReadLine();
+
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter State ");
+            string stateProvince = Console.ReadLine();
+
+            Console.WriteLine("Enter ZipPostal");
+            string zipPostal = Console.ReadLine();
+
+            Console.WriteLine("Enter Contry");
+            string country = Console.ReadLine();
+
+
+
+            Console.WriteLine("Student Information:");
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+            Console.WriteLine("Birthdate: " + birthdate);
+            Console.WriteLine("Address Line 1: " + addressLine1);
+            Console.WriteLine("Address Line 2: " + addressLine2);
+            Console.WriteLine("City: " + city);
+            Console.WriteLine("State/Province: " + stateProvince);
+            Console.WriteLine("Zip/Postal: " + zipPostal);
+            Console.WriteLine("Country: " + country);
+
+
+
+
+        }
+    }
+}
---
+++ hello/hello/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////////
+            //int n;
+            //Console.Write("Enter Your Number ");
+            //n = int.Parse(Console.ReadLine());
+            //if (n % 2 == 0)
+            //{
+            //    Console.WriteLine("Even Number ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd Number ");
+            //}
+
+            /////////////////////////////////
+            //int k;
+            //Console.Write("Enter Year");
+            //k= int.Parse(Console.ReadLine());
+            //if (k % 400 == 0 || k % 4 == 0)
+            //{
+            //    Console.WriteLine("Leap Yaer");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not A leap Yaer");
+            //}
+
+            /////////////////////////////////////
+
+            int n;
+            n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                //Console.WriteLine(i);
+                for (int j = 0; j < n - i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
